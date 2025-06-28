1d8f33cd4f5d45b9c88ea2ceccc3e3450bedd8bf
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-28 18:45:05 +0000

    Setup Spark and Glue for ETL Products Script

--- glue_jobs/etl_products.py
+++ glue_jobs/etl_products.py
@@ -2,3 +2,11 @@ import os
 import boto3
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+
+# Setup Spark & Glue
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session.builder \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .getOrCreate()
