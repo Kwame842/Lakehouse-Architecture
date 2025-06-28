c64857976c681d8434321b6500d474664935e124
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-28 19:01:51 +0000

    Set S3 paths and configuration for ETL Order items Script

--- glue_jobs/etl_order_items.py
+++ glue_jobs/etl_order_items.py
@@ -11,3 +11,9 @@ spark = glueContext.spark_session.builder \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
     .getOrCreate()
+
+# S3 paths and config
+bucket = "ecom-lakehouse"
+raw_prefix = "lakehouse/raw/order_items/"
+processed_path = f"s3://{bucket}/lakehouse/processed/order_items/"
+log_prefix = "lakehouse/processed/_processed_log/order_items/"
