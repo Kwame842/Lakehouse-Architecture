515b85c299d8d97134963518687c64cb423f2093
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-28 19:01:09 +0000

    Initialize Spark and Glue

--- glue_jobs/etl_order_items.py
+++ glue_jobs/etl_order_items.py
@@ -3,3 +3,11 @@ import boto3
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from pyspark.sql.functions import col, to_date
+
+# Initialize Spark and Glue
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session.builder \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .getOrCreate()
