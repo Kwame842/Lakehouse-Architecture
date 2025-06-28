b366813faa744f67591d4824d47021e2ea500ba5
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-28 18:45:42 +0000

    Set S3 paths for ETL Products Script

--- glue_jobs/etl_products.py
+++ glue_jobs/etl_products.py
@@ -10,3 +10,9 @@ spark = glueContext.spark_session.builder \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
     .getOrCreate()
+
+# S3 Paths
+bucket = "ecom-lakehouse"
+raw_key = "uploads/products.csv"
+processed_path = f"s3://{bucket}/lakehouse/processed/products"
+log_key = "lakehouse/processed/_processed_log/products/products.txt"
