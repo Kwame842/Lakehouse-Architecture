5a417d4e22f9b42014444336f6ae23e140037a92
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-28 18:53:23 +0000

    Setup Spark and Glue for ETL Orders Script

--- glue_jobs/etl_orders.py
+++ glue_jobs/etl_orders.py
@@ -3,3 +3,12 @@ import boto3
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from pyspark.sql.functions import col, to_date
+
+
+# Initialize Spark and Glue context
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session.builder \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .getOrCreate()
